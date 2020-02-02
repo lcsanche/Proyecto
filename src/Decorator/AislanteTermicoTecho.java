5fd82ee44fc502d58b85cdd8669cf535f6b19ea1
krortega@espol.edu.ec <DELL@PC-MISAEL>
2020-02-01 23:30:32 -0500

    Se implementa funciones con el patron Decorator para los acabados adicionales

---
+++ src/Decorator/AislanteTermicoTecho.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Decorator;
+
+/**
+ *
+ * @author DELL
+ */
+public class AislanteTermicoTecho implements Acabado{
+
+    @Override
+    public void anadirDecoracion() {
+       this.anadirDecoracion();
+       
+    }
+    
+}
---
+++ src/Decorator/IluminacionLed.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Decorator;
+
+/**
+ *
+ * @author DELL
+ */
+public class IluminacionLed implements Acabado{
+
+    @Override
+    public void anadirDecoracion() {
+        this.anadirDecoracion();
+    }
+    
+}
